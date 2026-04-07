830f0cf6632ad974444cf3f04e8c0cd76b89cd8d
Thauã <thaua23sl@gmail.com>
2026-04-07 13:32:52 -0300

    remake catalogos

---
+++ js/catalogo.js
@@ -0,0 +1,7 @@
+function openModal(rio) {
+   document.getElementById("modal-" + rio).style.display = "flex";
+}
+
+function closeModal(rio) {
+   document.getElementById("modal-" + rio).style.display = "none";
+}
